4b630fd9ab05bd08369e99e9a56f35091d2ae311
KNTXTremE <kan.horwang@gmail.com>
2020-06-23 17:39:37 +0700

    Init BackEnd part

---
+++ BackEnd/index.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 5000
+const HOSTNAME = process.env.IP || "localhost"
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res, next) {
+    res.send("This services are running on express-node.js");
+});
+
+app.use('/api', require('./routes/index'));
+
+app.listen(PORT,()=>console.log(`This app listening at http://${HOSTNAME}:${PORT}`))
---
+++ BackEnd/routes/index.js
@@ -0,0 +1,5 @@
+var router = require('express').Router();
+
+router.use('/order', require('./order'));
+
+module.exports = router;
